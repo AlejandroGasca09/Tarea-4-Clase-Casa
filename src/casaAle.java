05b30c6860ecf09a86b5bf4129a13c43410643ca
AlejandroGasca09 <119777270+AlejandroGasca09@users.noreply.github.com>
2023-03-05 21:10:14 -0600

    Update casaAle.java

--- src/casaAle.java
+++ src/casaAle.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class casaAle {
+public class casaAle {
+    public static void main(String[] args) {
+        casa casa1 = new casa();
+        casa casa2 = new casa("CDMX","VERDE",true,200000000,4);
+        System.out.println(casa1.toString());
+        System.out.println(casa2.toString());
+
+
+    }
 }
